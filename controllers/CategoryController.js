d8ef015967ab8677f9fceea17ec380bfd53b6596
DanielMiguelez <danielmiguelez1993@gmail.com>
2022-11-02 19:03:36 +0100

    ENDPOINTS CATEGORIAS COMENTADOS

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -43,7 +43,48 @@ const CategoryController = {
         .send({ msg: "Ups, hubo un error al reventar la categoria", Category });
     }
   },
+  
+  // Punto 4 de CRUD CATEGORIAS---------------------------------
+  /*getAll(req, res) {
+    // Post.findAll({ include: [{ model: User, attributes: ["name"] }] })
+    Category.findAll()
+      .then((categories) => res.send(categories))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send({message: "Hubo un problema al traer las categorias"});
+      });
+  },*/
 
+  /*async getCategoryById(req, res) {
+    try {
+      const category = await Category.findByPk(req.params.id, {
+      });
+      res.send(category);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send({ msg: "Hubo un error al obtener la categoria", error });
+    }
+  },*/
+
+  /*async getCategoryByName(req, res) {
+    try {
+      const category = await Category.findOne({
+        where: {
+          name: {
+            [Op.like]: `%${req.params.name}%`,
+          },
+        },
+      });
+      res.send(category);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send({ msg: "Hubo un error al buscar la categoria por nombre", err });
+    }
+  },*/
 
 };
 
