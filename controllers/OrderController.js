5690a75269464914413ea40ec959b3fa83d62a76
DanielMiguelez <danielmiguelez1993@gmail.com>
2022-11-04 13:18:18 +0100

    Orders created ALL ENDPOINTS

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,29 +1,29 @@
-const { Order } = require("../models/index.js");
-
-/*const OrderController = {
-  create(req, res) {
-    req.body.role = "order";
-
-    Order.create(req.body)
-
-      .then((order) =>
-        res.status(201).send({ message: "Orden creada con éxito", order })
-      )
-
-      .catch(console.error);
-  },
-};*/
+const { Order, Product } = require("../models/index.js");
 
 const OrderController = {
   async createOrder(req, res){
     try{
-    const order = await Order.create (req.body)
-    Order.addProduct(req.body.ProductId)
+    const order = await Order.create ({...req.body,date: new Date(),
+      status: "in process"})
+    order.addProduct(req.body.ProductId)
     res.send({msg:"order successful", order})
   } catch (error){
       console.error(error)
-      res.status(500).send({msg:"Error in the process", error})
+      res.status(500).send({msg:"No me has creado la orden socio...", error})
   }
   },
+
+
+async getOrdersAndProducts(req, res) {
+  try {
+    const categories = await Order.findAll({
+      include: [{ model: Product, attributes: ["name","price"], through: { attributes: [] } }],
+    });
+    res.send({ msg: "Your Categories and products", categories });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ msg: "Error while getting Categories with products", error });
+  }}
+
 };
 module.exports = OrderController;
