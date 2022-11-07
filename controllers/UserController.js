76796aa9b0831ce39f09dd2c389b1e7048d0c0d2
diegopalones <diego.palones@gmail.com>
2022-11-07 13:42:33 +0100

    ultimo bug

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,8 @@
-const { User, Order,Product,Token, sequelize } = require("../models/index.js");
+const { User, Order,Product,Token, Sequelize } = require("../models/index.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { jwt_secret } = require("../config/config.json")["development"];
-const { Op } = sequelize;
+const { Op } = Sequelize;
 
 const UserController = {
   async create(req, res, next) {
